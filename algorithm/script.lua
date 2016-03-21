a9728aea9cd04d7ed40f90bc1f176318d7a859ea
danilolucas <daniloluca@live.com>
2016-03-20 23:42:39 -0300

    Criado algoritmo parcial de Machine Learning, nesta versão as reações só são executadas em inimigos

--- algorithm/script.lua
+++ algorithm/script.lua
@@ -8,6 +8,7 @@ local player = {
 	speed = 0x007b,
 	animationTrigger = 0x0071,
 	onAir = 0x0072,
+	reaction = 55,
 };
 
 local enemy = {
@@ -16,19 +17,49 @@ local enemy = {
 	x_high = 0x14e0,
 	x_low = 0x00e4,
 	y_high = 0x14d4,
-	y_low = 0x00d8
+	y_low = 0x00d8,
 }
 
-local enemies;
+local enemyReactions = {
+	[5] = {Y=false, right=false, A=true},
+	[0] = {Y=false, right=false, B=true},
+	[189] = {Y=false, right=false, A=true},
+	[171] = {Y=false, right=true, A=true},
+}
 
+local enemies = {};
+local closestEnemy = {};
+
+--functions
 local function signed(num, bits)
     local maxval = 2^(bits - 1);
     if num < maxval then return num else return num - 2*maxval end
 end
 
+local function console()
+	local count = 50;
+	for i=1, table.getn(enemies), 1 do
+		gui.text(110, count, "enemy: " .. tostring(enemies[i]));
+		count = count + 10;
+	end
+	gui.text(10, 200, "X: " .. s16(player.x));
+	gui.text(10, 210, "Y: " .. s16(player.y));
+end
 
+--[[local function getClosestEnemy()
+	local closest;
+	for i=1, table.getn(enemies), 1 do
+		if i==1 then 
+			closest = enemies[i];
+		end
+		if math.abs(enemies[i].x - s16(player.x)) < math.abs(closest.x - s16(player.x)) then
+			closest = enemies[i];
+		end
+	end
+	closestEnemy = closest;	
+end]]
 
-while true do
+local function getEnemies()
 	enemies = {};
 
 	for i=0, 12, 1 do
@@ -46,15 +77,26 @@ while true do
 			table.insert(enemies, e);
 		end
 	end
+end
 
-	local count = 50;
+local function playerAction()
+	joypad.set(1, {Y=true});
+	joypad.set(1, {right=true});
+
 	for i=1, table.getn(enemies), 1 do
-		gui.text(110, count, "enemy: " .. tostring(enemies[i]));
-		count = count + 10;
+		if math.abs(enemies[i].x - s16(player.x)) < player.reaction then
+			if enemyReactions[enemies[i].num] ~= nil then
+				joypad.set(enemyReactions[enemies[i].num]);
+			end
+		end
 	end
+end
 
-	gui.text(10, 200, "X: " .. s16(player.x));
-	gui.text(10, 210, "Y: " .. s16(player.y));
+--update
+while true do
+	getEnemies();
+	playerAction();
+	console();
 
 	emu.frameadvance();--important
 end
